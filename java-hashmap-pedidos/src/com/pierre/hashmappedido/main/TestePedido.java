e14f7e4415748173d59a6beab89ced103e5dcd3c
olive <olive@Oliveira>
2026-03-31 17:48:03 -0300

    Criado HashMap percorrido lista da categoria e impresso no console

--- java-hashmap-pedidos/src/com/pierre/hashmappedido/main/TestePedido.java
+++ java-hashmap-pedidos/src/com/pierre/hashmappedido/main/TestePedido.java
@@ -1,6 +1,7 @@
 package com.pierre.hashmappedido.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.pierre.hashmappedido.model.Pedido;
@@ -9,11 +10,11 @@ public class TestePedido {
 	
 	public static void main(String[] args) {
 		
-		Pedido pedido01 = new Pedido("Marcos", "ENTREGRE", 150.53, 1012);
+		Pedido pedido01 = new Pedido("Marcos", "ENTREGUE", 150.53, 1012);
 		Pedido pedido02 = new Pedido("Luana", "ENVIADO", 301.98, 1023);
 		Pedido pedido03 = new Pedido("Pierre", "PENDENTE", 56.50, 1005);
 		Pedido pedido04 = new Pedido("Bernardo", "ENVIADO", 100.00, 1016);
-		Pedido pedido05 = new Pedido("Cida", "ENVIADO", 201.60, 021);
+		Pedido pedido05 = new Pedido("Cida", "ENVIADO", 201.60, 1021);
 		Pedido pedido06 = new Pedido("Rogerio", "ENTREGUE", 360.20, 1022);
 		Pedido pedido07 = new Pedido("Paulo", "PENDENTE", 111.35, 1017);
 		Pedido pedido08 = new Pedido("Inês", "ENVIADO", 78.00, 1008);
@@ -31,7 +32,25 @@ public class TestePedido {
 		pedidos.add(pedido08);
 		pedidos.add(pedido09);
 		
+		HashMap<String, List<Pedido>> statusPedido = new HashMap<>();
 		
+		statusPedido.put("ENVIADO", new ArrayList<>());
+		statusPedido.put("PENDENTE", new ArrayList<>());
+		statusPedido.put("ENTREGUE", new ArrayList<>());
+		
+		for (Pedido pedido : pedidos) {
+			statusPedido.get(pedido.getStatus()).add(pedido);			
+		}
+		
+		for (String status : statusPedido.keySet()) {
+			System.out.println("******** " + status + " ********");
+			
+			for (Pedido pedido : statusPedido.get(status)) {
+				System.out.println("Nome: " + pedido.getCliente() + " | Pedido: " + pedido.getNumeroPedido() + " | Valor: R$ " 
+						+ pedido.getValor() );
+			}
+			System.out.println();
+		}
 		
 	}
 
